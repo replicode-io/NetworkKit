fee808233106d5bc78669cfe61ccdf6a937fbf7e
RobCanton <robshanecanton@gmail.com>
2025-08-25 17:04:33 -0400

    added public init

--- Sources/NetworkKit/Models/Endpoint.swift
+++ Sources/NetworkKit/Models/Endpoint.swift
@@ -1,9 +1,17 @@
 import Foundation
 
-public  struct Endpoint {
+public struct Endpoint {
     let host:Host
     let path: String
     var url:URL {
         return URL(string: "\(host.name)\(path)")!
+    }
+    
+    public init(
+        host: Host,
+        path: String
+    ) {
+        self.host = host
+        self.path = path
     }
 }
--- Sources/NetworkKit/Models/Host.swift
+++ Sources/NetworkKit/Models/Host.swift
@@ -1,4 +1,12 @@
 public struct Host {
     let name: String
     let authorization:() async -> Authorization?
+    
+    public init(
+        name: String,
+        authorization: @escaping () async -> Authorization?
+    ) {
+        self.name = name
+        self.authorization = authorization
+    }
 }
